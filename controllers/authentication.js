b8173039b2ba635305eb07237f7d7c309bd79874
Miha Kozina <mk4571@stundent.uni-lj.si>
2018-04-10 20:31:34 +0600

    Add forgotten password feature.

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bcrypt = require('bcrypt');
+var crypto = require('crypto');
+var nodemailer = require('nodemailer');
 const saltRounds = 10;
 
 var db = require('../controllers/db');
@@ -56,36 +58,36 @@ function forgot(req, res) {
           if(token) {
             user.resetToken = token;
             db.updateUser(user, () => {
-              var smtpTransport = nodemailer.createTransport('SMTP', {
-                service: 'Gmail',
-                auth: {
-                  user: 'studis.tpo.2018',
-                  pass: 'secretpass1'
-                }
-              });
+              var smtpTransport = nodemailer.createTransport('smtps://studis.tpo.2018%40gmail.com:secretpass1@smtp.gmail.com');
               var mailOptions = {
                 to: user.email,
-                from: 'passwordreset@demo.com',
-                subject: 'Node.js Password Reset',
+                from: 'studis.tpo.2018@gmail.com',
+                subject: 'TPO Studis Password Reset',
                 text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
                   'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
                   'http://' + req.headers.host + '/reset/' + token + '\n\n' +
                   'If you did not request this, please ignore this email and your password will remain unchanged.\n'
               };
               smtpTransport.sendMail(mailOptions, function(err) {
-                redirect('/');
+                if(err) {
+                    console.log(err);
+                } else {
+                    console.log('Pass reset email sent.');
+                }
+                res.redirect('/');
               });
             })
           } else {
-            res.redirect('forgot');
+            res.redirect('/forgot');
           }
         });
       } else {
-        res.redirect('forgot');
+        res.redirect('/forgot');
       }
     });
+  } else {
+    res.redirect('/forgot');
   }
-  res.redirect('login');
 }
 
 function generateToken(callback) {
@@ -95,6 +97,22 @@ function generateToken(callback) {
   });
 }
 
+function reset(req, res) {
+  db.getUser({ resetToken: req.params.token}, (user) => {
+    if(!user || req.body.password != req.body.confirm) {
+      res.redirect('/');
+    } else {
+      user.password = bcrypt.hashSync(req.body.password, saltRounds);
+      user.resetToken = undefined;
+      
+      db.updateUser(user, () => {
+        res.redirect('/');
+      })
+    }
+  })
+}
+
 exports.authenticate = authenticate;
 exports.login = login;
 exports.forgot = forgot;
+exports.reset = reset;
