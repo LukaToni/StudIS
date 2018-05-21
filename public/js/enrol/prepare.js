8d1bfdf6d3e0e5a64a65b8326c04c82126bb3f87
Klemen <ks8288@student.uni-lj.si>
2018-05-21 19:54:00 +0200

    manjši fix pri vpisu glede vnašanja polj

--- public/js/enrol/prepare.js
+++ public/js/enrol/prepare.js
@@ -8,4 +8,53 @@ function onEmsoChange(){
     //debugger;
 }
 
+function onDataChange(){
+    ulica = $('#staticStreet').val();
+    ime = $('#staticName').val();
+    priimek = $('#staticSurname').val();
+    emso = $('#staticEMSO').val();
+    telefon = $('#staticPhone').val();
+    ulicaPosta = $('#staticStreetPost').val();
+
+    if(ulica && ime && priimek && emso && telefon){
+        $('#submitButton').prop("disabled", false);
+    }
+    else{
+        $('#submitButton').prop("disabled", true);
+    }
+}
+
 $('#staticEMSO').on('change',onEmsoChange);
+
+$('#staticStreet').on('change',onDataChange);
+$('#staticName').on('change',onDataChange);
+$('#staticSurname').on('change',onDataChange);
+$('#staticEMSO').on('change',onDataChange);
+$('#staticPhone').on('change',onDataChange);
+
+var ulica = $('#staticStreet').val();
+var ime = $('#staticName').val();
+var priimek = $('#staticSurname').val();
+var emso = $('#staticEMSO').val();
+var telefon = $('#staticPhone').val();
+var ulicaPosta = $('#staticStreetPost').val();
+
+    //$('#submitButton').prop("disabled", false);
+/*
+var indeks = document.getElementById('country');
+var drzava = $('#staticCountry').val();
+if(drzava != 'Slovenija')
+    $('#staticCountry').css('display','none');
+else
+    $('#staticCountry').css('display','');
+*/
+// && ime && priimek && emso && telefon
+var a;
+if(ulica && ime && priimek && emso && telefon){
+    a=1;
+    $('#submitButton').prop("disabled", false);
+}
+else{
+    a=2;
+    $('#submitButton').prop("disabled", true);
+}
