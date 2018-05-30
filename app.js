559c69b43c1305e05c5ba4a7f24e4a8b0add914c
Klemen <ks8288@student.uni-lj.si>
2018-05-30 22:27:45 +0200

    fix, da laufa tist kar je prej

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var personalRouter = require('./routes/personal_data');
 //var studentImportRouter = require('./routes/student_import');
-var studentEnrolsRouter = require('./routes/course_enrols');
+//var studentEnrolsRouter = require('./routes/course_enrols');
 var countCourseEnrols = require('./routes/count_course_enrols');
 var studentImportRouter = require('./routes/student_import');
 var tokens = require('./routes/tokens');
@@ -49,7 +49,7 @@ app.use('/tokens', tokens);
 app.use('/vpisni_to_pdf', vpisniPdfRouter);
 app.use('/edit_exams', editExamsRouter);
 //app.use('/student_import', studentImportRouter);
-app.use('/student_enrols', studentEnrolsRouter);
+//app.use('/student_enrols', studentEnrolsRouter);
 app.use('/count_course_enrols', countCourseEnrols);
 app.use('/exam_list', require('./routes/exam_list'))
 app.use('/', loginRouter);
